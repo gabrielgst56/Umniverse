07790c5761f9974df19c1d87710cc20052f39d7e
Gabriel Augusto <gabrielgst56@hotmail.com>
2019-06-02 19:26:37 -0300

    Inicio do projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Umniverse';
+  texto : string = 'IFSP Campinas';
+  lat: number = -22.8505229;
+  lng: number = -47.1278806;
+  zoom: number = 15;
+}
